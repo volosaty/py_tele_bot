7b90044367fe2c98abea1c93ca75588dfc4f65d3
volosaty <volosaty@gmail.com>
2016-10-01 06:23:31 +0300

    3

--- bot.py
+++ bot.py
@@ -16,15 +16,20 @@ bot = telebot.TeleBot(token)
 
 proc = subprocess.Popen(["df", "-h"], stdout=subprocess.PIPE)
 output = proc.stdout.read()
+
 proc = subprocess.Popen(["free", "-g"], stdout=subprocess.PIPE)
 output1 = proc.stdout.read()
+
 proc = subprocess.Popen(["uptime"], stdout=subprocess.PIPE)
 output2 = proc.stdout.read()
+
 proc = subprocess.Popen(["cat", "list_txt"], stdout=subprocess.PIPE)
 output4 = proc.stdout.read()
 
-def add(x, y):
-    return x + y
+proc = subprocess.Popen(["./news.sh"], stdout=subprocess.PIPE)
+output5 = proc.stdout.read()
+
+
 
 # Handles all text messages that contains the commands '/start' or '/help'.
 @bot.message_handler(commands=['fuck'])
@@ -37,8 +42,10 @@ def server_free(message):
     bot.reply_to(message, text=output1)
     pass
 
-
-
+@bot.message_handler(commands=['news'])
+def server_free(message):
+    bot.reply_to(message, text=output5)
+    pass
 
 
 if __name__ == '__main__':
